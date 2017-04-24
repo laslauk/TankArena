a531dcd9d5d8de74fe5bad3cb6d9e5fe132a2834
Lasse Laukkanen <larpatin@gmail.com>
2017-04-24 16:35:38 +0300

    Created the Unreal Project

---
+++ TankArena/Source/TankArena/TankArenaGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "TankArenaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKARENA_API ATankArenaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
